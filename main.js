bd167711c14a7721ff7ba7f55514dda14f2f4c97
tonia domiciano <133991203+toniacds@users.noreply.github.com>
2023-11-21 14:25:36 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
-function allowDrop(ev){
+function allowDrop(ev) 
+{
     ev.preventDefault();
 }
 
-function drag(ev){
+function drag(ev) 
+{
     ev.dataTransfer.setData("text", ev.target.id);
+    ev.dataTransfer.effectAllowed = "copy";
 }
 
-function drop(ev){
+function drop(ev) 
+{
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
+    var copyimg = document.createElement("img");
+    var original = document.getElementById(data);
+    copyimg.src = original.src;
+    ev.target.appendChild(copyimg);
 }
